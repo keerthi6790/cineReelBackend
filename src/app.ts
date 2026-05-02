e05cf702759adf65a5d0f56e872535584100b32d
keerthivasan.b1-v <keerthivasan.b1-v@abfrl.adityabirla.com>
2026-05-02 20:29:56 +0530

    host added

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ const server = buildServer();
 
 async function main() {
   try {
-    await server.listen({ port: 3000 });
+    await server.listen({ port: 3000, host: "0.0.0.0" });
 
     console.log("Process running on http://localhost:3000");
   } catch (e) {
